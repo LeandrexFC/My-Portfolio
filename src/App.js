5d3b8a6dbb41042c1b513af08da67b037355be83
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-01-09 18:03:33 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter, Route } from 'react-router-dom';
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -18,7 +18,5 @@ function App() {
     </HashRouter>
   );
 }
-
-const Home = () => (<div> test </div>)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<HashRouter>
-  <App />
-</HashRouter>);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
