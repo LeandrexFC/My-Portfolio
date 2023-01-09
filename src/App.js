8d7ff1377b7524cbbb3d2fca31b6c9a5e6bd60f7
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-01-09 15:23:19 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Skills from "./pages/Skills";
 
+
 function App() {
   return (
-    <Switch>
+    <HashRouter basename="/">
     <Route exact path="/" component={ Home } />
     <Route exact path="/projects" component={ Projects } />
     <Route exact path="/about" component={ About } />
     <Route exact path="/contact" component={ Contact } />
     <Route exact path="/skills" component={ Skills } />
-    </Switch>
+    </HashRouter>
   );
 }
 
