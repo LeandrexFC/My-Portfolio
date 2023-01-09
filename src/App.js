d76021fa5b7df0faedc0e37a05277e3eea0e26a4
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-01-09 17:56:58 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter, Route } from 'react-router-dom';
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -10,13 +10,15 @@ import Skills from "./pages/Skills";
 function App() {
   return (
     <HashRouter basename="/">
-    <Route  path="/" component={ Home } />
-    <Route  path="/projects" component={ Projects } />
-    <Route  path="/about" component={ About } />
-    <Route  path="/contact" component={ Contact } />
-    <Route  path="/skills" component={ Skills } />
+    <Route exact path="/" component={ Home } />
+    <Route exact path="/projects" component={ Projects } />
+    <Route exact path="/about" component={ About } />
+    <Route exact path="/contact" component={ Contact } />
+    <Route exact path="/skills" component={ Skills } />
     </HashRouter>
   );
 }
+
+const Home = () => (<div> test </div>)
 
 export default App;
