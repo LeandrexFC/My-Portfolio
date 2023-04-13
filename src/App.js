3533454cbc2c6c6f768180c8f77623042254532d
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-04-13 19:00:38 -0300

    feat: add deal wrong url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { HashRouter, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Skills from "./pages/Skills";
+import NotFound from "./Components/NotFound";
+import { createBrowserHistory } from 'history';
 
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <HashRouter basename="/">
-    <Route exact path="/" component={ Home } />
-    <Route exact path="/projects" component={ Projects } />
-    <Route exact path="/about" component={ About } />
-    <Route exact path="/contact" component={ Contact } />
-    <Route exact path="/skills" component={ Skills } />
-    </HashRouter>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/skills" component={Skills} />
+        <Route component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Erro 404 - Página não encontrada</h1>
+      <p>Desculpe, mas a página que você está procurando não existe.</p>
+      <img src="https://www.shutterstock.com/shutterstock/videos/1092123327/thumb/1.jpg?ip=x480" alt="Imagem de erro" />
+    </div>
+  );
+}
+
+export default NotFound;
