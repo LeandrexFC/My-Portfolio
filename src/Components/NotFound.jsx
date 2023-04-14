49377ff3c406f434bc321487ea3f4d128fe3d739
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-04-14 20:28:00 -0300

    feat: add easter egg

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../AllCss/NotFound.css'
-import neymarImg from '../images/neymar.jpeg'
+ import neymarImg from '../images/neymar5.jpg'
 import Header from '../Components/Header'
 
 
@@ -9,7 +9,7 @@ function NotFound() {
     <> 
       <Header />
     <div className="errorDiv">
-      <img src={ neymarImg } alt="neymar img"/>
+    <img src={ neymarImg } />
       <div className="allContent">
  
         <h1 className="errorTitle h" >Error 404 - Page Not Found</h1>
